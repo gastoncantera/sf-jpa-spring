a7c6ccd64908b022596e33cbf73c470d14029131
Gaston Cantera <65862015+gastoncantera@users.noreply.github.com>
2023-12-05 16:50:40 -0300

    Initial commit

---
+++ src/test/java/com/example/sfjpaspring/SfJpaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sfjpaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfJpaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
