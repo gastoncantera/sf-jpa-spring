b11860425ffc5e9e76cfa018da224076a749dc29
Gaston Cantera <65862015+gastoncantera@users.noreply.github.com>
2023-12-06 19:07:35 -0300

    Multiple data source support in JPA Data

--- src/main/java/com/example/sfjpaspring/repository/SpringDataJpaEmployeeRepository.java
+++ src/main/java/com/example/sfjpaspring/repository/SpringDataJpaEmployeeRepository.java
@@ -2,6 +2,19 @@ package com.example.sfjpaspring.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.sfjpaspring.entity.Employee;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface SpringDataJpaEmployeeRepository extends JpaRepository<Employee, Integer> {
+    @Override
+    @Transactional("readOnlyTransactionManager")
+    List<Employee> findAll();
+
+    @Override
+    @Transactional("readOnlyTransactionManager")
+    Optional<Employee> findById(Integer id);
 }
